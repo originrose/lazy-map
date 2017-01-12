51b391b209f7cea34de4c6430db20ca6d40b4ecb
Radon Rosborough <radon.neon@gmail.com>
2017-01-12 13:16:38 -0700

    Bump version to 0.1.0 for deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/lazy-map "0.1.0-SNAPSHOT"
+(defproject thinktopic/lazy-map "0.1.0"
   :description "Lazy maps for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
