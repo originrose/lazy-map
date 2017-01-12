bae64b40f8363b94d4207d9448f5803df1f866d5
Radon Rosborough <radon.neon@gmail.com>
2017-01-12 13:09:47 -0700

    Only set :main in :repl profile

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main lazy-map.core)
+  :profiles {:repl {:main lazy-map.core}})
